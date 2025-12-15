63959a600c92027dc30b0207475424b36ffbb3ab
Conan Quilty <conanhq03@gmail.com>
2025-12-15 21:20:30 +0000

    Express Lab - exercise done

--- tasky-api/api/tasks/index.js
+++ tasky-api/api/tasks/index.js
@@ -22,12 +22,14 @@ router.get('/:id', (req, res) => {
 //Add a task
 router.post('/', (req, res) => {
     const { title, description, deadline, priority, done } = req.body;
+    const timeStamp = new Date().toString();
     const newTask = {
         id: uuidv4(),
         title,
         description,
         deadline,
         priority,
+        created_at: timeStamp,
         done
     };
     tasksData.tasks.push(newTask);
@@ -42,7 +44,8 @@ router.put('/:id', (req, res) => {
     if (taskIndex === -1) {
         return res.status(404).json({ status: 404, message: 'Task not found' });
     }
-    const updatedTask = { ...tasksData.tasks[taskIndex], ...req.body, id:id };
+    const timeStamp = new Date().toString();
+    const updatedTask = { ...tasksData.tasks[taskIndex], ...req.body, id:id, updated_at: timeStamp };
     tasksData.tasks[taskIndex] = updatedTask;
     res.json(updatedTask);
 });
