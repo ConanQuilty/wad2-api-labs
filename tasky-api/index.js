626ebd810f9efa7d4d020bc01583d3ca80ca95ae
Conan Quilty <conanhq03@gmail.com>
2025-12-17 15:44:24 +0000

    Added Users Router

--- tasky-api/index.js
+++ tasky-api/index.js
@@ -4,12 +4,15 @@ import tasksRouter from './api/tasks';
 import './db';
 // other imports
 import cors from 'cors';
+//... other imports
+import usersRouter from './api/users';
+
 
 
 
 dotenv.config();
 
-const errHandler = (err, req, res, /*next*/) => {
+const errHandler = (err, req, res, next) => {
   /* if the error in development then send stack trace to display whole error,
   if it's in production then just send error message  */
   if(process.env.NODE_ENV === 'production') {
@@ -29,6 +32,9 @@ app.use('/api/tasks', tasksRouter);
 app.use(errHandler);
 // Enable CORS for all requests
 app.use(cors());
+//Users router
+app.use('/api/users', usersRouter);
+
 
 
 
