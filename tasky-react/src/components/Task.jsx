169021a0c859fcb5e331a5b761dafd689a729772
Conan Quilty <conanhq03@gmail.com>
2025-12-20 19:06:31 +0000

    Authentication front-end set up

---
+++ tasky-react/src/components/Task.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Task = (props) => {
+    
+    return (
+        <div className="card" style={{backgroundColor: props.done ? 'lightGrey' : '#5bb4c4'}}>
+            <p className="title">{props.title}</p>
+            <p>Due: {props.deadline}</p>
+            <p className="description">{props.description}</p>
+            <p className={`priority ${props.priority.toLowerCase()}`}>{props.priority}</p>
+            <button onClick={props.markDone} className='doneButton'>Done</button>
+            <button onClick={props.deleteTask} className="deleteButton">Delete</button>
+        </div>
+    )
+}
+
+export default Task;
